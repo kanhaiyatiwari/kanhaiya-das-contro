245b961fb210ec63a155942ff0c8d52eeaaad20f
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-08-21 11:26:39 +0530

    add

--- Intro_of_dsa/src/com/dsar/ricursion/Factorial.java
+++ Intro_of_dsa/src/com/dsar/ricursion/Factorial.java
@@ -3,7 +3,9 @@ package com.dsar.ricursion;
 public class Factorial {
 //finad the factoral of a number 
 	
-	for examle if num=5;
-		5*4*3*2*1=120
-				here we can clearly say that if
+//	for examle if num=5;
+//		5*4*3*2*1=120
+//				here we can clearly say that if we want fac=5 then we need fac of 4
+//				and so on;
+
 }
