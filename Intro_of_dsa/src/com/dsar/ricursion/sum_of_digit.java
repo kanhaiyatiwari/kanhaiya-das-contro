101031a212ef3a7c9d28073e9e99528187533630
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-08-21 11:42:18 +0530

    add

---
+++ Intro_of_dsa/src/com/dsar/ricursion/sum_of_digit.java
@@ -0,0 +1,21 @@
+package com.dsar.ricursion;
+
+public class sum_of_digit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		question is find the sum of digits 
+//		if disitst are 1234=10;
+//		here we can also divide the problem into sub parts;
+//		if sum of 1 give me the sum of other part 234;
+//		1* sum(234);
+//		12* sum(34);
+//		123* sum(4);
+//		1234;
+	}
+	public static int sumOfDigit(int n) {
+		
+	}
+
+}
