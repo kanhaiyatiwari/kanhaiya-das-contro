c9c9bf537c0340ec1b0274cc35333996543a974a
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-08-21 11:35:23 +0530

    add

--- Intro_of_dsa/src/com/dsar/ricursion/Factorial.java
+++ Intro_of_dsa/src/com/dsar/ricursion/Factorial.java
@@ -1,5 +1,7 @@
 package com.dsar.ricursion;
 
+import java.util.Scanner;
+
 public class Factorial {
 //finad the factoral of a number 
 	
@@ -7,5 +9,22 @@ public class Factorial {
 //		5*4*3*2*1=120
 //				here we can clearly say that if we want fac=5 then we need fac of 4
 //				and so on;
+//5*fac(4)
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("please inter a number");
+		int n=s.nextInt();
+		
+		System.out.println(fac(n));
+	}
 
+private static int fac(int n) {
+	// TODO Auto-generated method stub
+if(n<=1) {
+	return 1;
 }
+return n*fac(n-1);
+}
+}
+
