6add81b28e3c2b33408ab66cd6e3f58432020259
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-08-21 12:00:01 +0530

    add

--- Intro_of_dsa/src/com/dsar/ricursion/sum_of_digit.java
+++ Intro_of_dsa/src/com/dsar/ricursion/sum_of_digit.java
@@ -1,5 +1,7 @@
 package com.dsar.ricursion;
 
+import java.util.Scanner;
+
 public class sum_of_digit {
 
 	public static void main(String[] args) {
@@ -13,9 +15,17 @@ public class sum_of_digit {
 //		12* sum(34);
 //		123* sum(4);
 //		1234;
+		System.out.println("please inter dis=gits");
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+	
+		System.out.println(sumOfDigit(n));
 	}
 	public static int sumOfDigit(int n) {
-		
+		if(n==0) {
+			return 0;
+		}
+		return n%10+sumOfDigit(n/10);
 	}
 
 }
